45cb384f485911e70626387fbc933443963ffdf3
Samuel Vieira <samuel.vieira@bit.com.br>
2022-10-31 18:40:36 -0300

    primeiro commit sobre os conceitos de dart

---
+++ lista-map.dart
@@ -0,0 +1,11 @@
+void main() {
+  Map usuarios = {
+    'nome': 'Samuel',
+    'idade': 20,
+    'altura': 1.89,
+    'samueldev': true,
+  };
+
+  print(usuarios);
+  print(usuarios.length);
+}
---
+++ variaveis.dart
@@ -0,0 +1,13 @@
+void main() {
+  int idade = 20;
+  print(idade);
+  double altura = 1.88;
+  print(altura);
+  bool samuelDev = true;
+
+  if (samuelDev == true) {
+    print('É Dev!');
+  } else {
+    print('Não é dev');
+  }
+}
